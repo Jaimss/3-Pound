e0f3ae0961225a4ffe7853fb2d8607757bd01bca
Jaimss <james@harrell365.com>
2020-09-05 22:45:41 -0400

    starting off

---
+++ linreg-bicycle/bicycle.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.linear_model import LinearRegression
+
+plt.rcParams['figure.figsize'] = (20.0, 10.0)
+
+predict = 'cnt'
+
+# get the data
+data = pd.read_csv('./hour.csv')
+data = data[['season', 'yr', 'mnth', 'hr', 'temp', 'workingday', 'hum', 'windspeed', 'weathersit', 'cnt']]
+x = np.array(data.drop([predict], 1))  # the data to make prediction
+y = np.array(data[predict])  # the counts of people who rode a bike
+
+# get train and test sets of data for x and y
+x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.1)
+
+model = LinearRegression().fit(x_train, y_train)
+x_pred = model.predict(x_test)
+acc = model.score(x_test, y_test)
+print(acc)
